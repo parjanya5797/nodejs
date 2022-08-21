94263425c7f732a95ca2d2157e34ea406d33f1af
Parjanya Shakya <parjanya57@gmail.com>
2022-08-21 21:52:02 +0545

    First Commit

---
+++ routes/api/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router()
+// const uuid = require('uuid')
+let users = require('../../Users')
+
+//get all users
+router.get('/',(req,res)=>{
+    res.json(users);
+})
+router.get('/:id',(req,res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+
+    if(found)
+    {
+        res.json(users.filter(user => user.id === parseInt(req.params.id)))
+    }
+    else
+    {
+        res.sendStatus(400);
+    }
+})
+
+module.exports = router;
